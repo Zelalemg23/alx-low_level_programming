27ad6a2c6103af691f6a5d99d9108ea9d04ec73b
Zelalem23 <ethiozelalem23@gmail.com>
2023-10-20 05:57:45 -0700

    updated

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,30 @@
 section .data
-    format db 'Hello, Holberton', 0
+    hello_msg db "Hello, Holberton", 0
+    fmt db "%s", 0
 
 section .text
-    global main
     extern printf
 
-main:
-    mov edi, format
+global _start
+
+_start:
+    push rbp
+    mov rbp, rsp
+
+    ; Push the format string and the message onto the stack
+    mov rdi, fmt
+    mov rsi, hello_msg
+
+    ; Call printf
     xor eax, eax
     call printf
-    mov eax, 0
-    ret
+
+    ; Clean up the stack
+    xor eax, eax
+    mov rsp, rbp
+    pop rbp
+
+    ; Exit the program
+    xor eax, eax
+    mov ebx, 0x60
+    int 0x80
