5bc7a5aecd58720f57954c6d44adf2c59028b689
Zelalem23 <ethiozelalem23@gmail.com>
2023-12-08 10:52:36 -0800

    double

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,16 +6,17 @@
  * @h: Pointer to the head of the list.
  * Return: The number of nodes.
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
+	while (h != NULL)
+	{
+	printf("%d\n", h->n);
+	h = h->next;
+	count++;
+	}
 
-    return count;
+	return (count);
 }
